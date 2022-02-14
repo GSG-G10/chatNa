30c1e44870eaa455de19e496a83514d0805fbaef
nizar7zak <nizar7zakout@gmail.com>
2022-02-14 15:46:48 +0200

    App image component

--- app/screens/Welcome.js
+++ app/screens/Welcome.js
@@ -3,3 +3,5 @@ import { View, ImageBackground, StyleSheet } from "react-native";
 import colors from '../config/colors';
 import {AppText} from '../components/AppText';
 import { AppButton } from "../components/AppButton";
+import { AppImage } from "../components/AppImage";
+
