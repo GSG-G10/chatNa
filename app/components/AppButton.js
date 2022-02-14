ebf1d2f1a317ef316229e4ca6ef58ff6823579ca
nizar7zak <nizar7zakout@gmail.com>
2022-02-14 15:45:42 +0200

    App button component

---
+++ app/components/AppButton.js
@@ -0,0 +1,29 @@
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+import colors from "../config/colors";
+import { AppText } from "./AppText";
+
+export const AppButton = ({ title, onPress, style }) => {
+  return (
+    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+      <AppText style={styles.text}>{title}</AppText>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  text: {
+    color: colors.white,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
--- app/screens/Welcome.js
+++ app/screens/Welcome.js
@@ -1,3 +1,5 @@
 import { View, ImageBackground, StyleSheet } from "react-native";
 
 import colors from '../config/colors';
+import {AppText} from '../components/AppText';
+import { AppButton } from "../components/AppButton";
