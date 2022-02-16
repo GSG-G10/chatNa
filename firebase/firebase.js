bf22100f998ccb210755365ee128d6cc6783db96
reemsh2000 <sh951652@gmail.com>
2022-02-16 17:02:05 +0200

    firebase config

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
+import { initializeApp, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGDpi03nh-7yK8FoZ3_41MNSrU59sfd7Y",
@@ -13,11 +11,9 @@ const firebaseConfig = {
   appId: "1:929695673221:web:74e664b6c882c5d0b1b2ec",
   measurementId: "G-6MKYD5RLRB",
 };
-
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const authentication = getAuth(app)
+initializeApp(firebaseConfig);
+const app = getApp();
+const auth = getAuth();
 
-// const authentication = getAnalytics(app);
-export default authentication;
+export  { app, auth };
