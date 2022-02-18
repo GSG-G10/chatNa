6d4b84ff4e608e98ffff577acc2c08dbe043a775
reemsh2000 <sh951652@gmail.com>
2022-02-18 21:32:43 +0200

    delete useless component

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import navigationTheme from "./app/components/routes/navigationTheme";
-import { navigationRef } from "./app/components/routes/rootNavigation";
 import colors from './app/config/colors';
 import { AuthNavigator } from "./app/components/routes/authNavigator";
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavigationContainer theme={navigationTheme} ref={navigationRef}>
+      <NavigationContainer theme={navigationTheme}>
          <AuthNavigator />
         </NavigationContainer>
       <StatusBar style="auto" />
